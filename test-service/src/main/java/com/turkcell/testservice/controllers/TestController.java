db39763766edcee121927c8c593f07f88d8a4309
emrealkan884 <Hasan.alkan884@gmail.com>
2023-11-13 20:11:56 +0300

    Api Gateway and Eureka Server

---
+++ test-service/src/main/java/com/turkcell/testservice/controllers/TestController.java
@@ -0,0 +1,19 @@
+package com.turkcell.testservice.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/test")
+public class TestController {
+    @GetMapping("")
+    public String getMapping() {
+        return "GetMapping calisti.";
+    }
+    @PostMapping("")
+    public String postMapping() {
+        return "PostMapping calisti.";
+    }
+}
