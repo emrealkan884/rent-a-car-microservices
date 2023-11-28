742cf4c4f8e0a491dd19e85069c2aed28da13ad1
emrealkan884 <Hasan.alkan884@gmail.com>
2023-11-28 20:59:17 +0300

    Dockerize v2

--- api-gateway/src/main/java/com/turkcell/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/turkcell/apigateway/config/SecurityConfig.java
@@ -10,14 +10,18 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
-
   @Bean
   public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
     serverHttpSecurity
         .csrf(ServerHttpSecurity.CsrfSpec::disable)
         .authorizeExchange(
             exchange ->
-                exchange.pathMatchers("/eureka/**").permitAll().anyExchange().authenticated())
+                exchange
+                    // .pathMatchers("/eureka/**")
+                    .pathMatchers("/eureka/**")
+                    .permitAll()
+                    .anyExchange()
+                    .authenticated())
         .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
     return serverHttpSecurity.build();
