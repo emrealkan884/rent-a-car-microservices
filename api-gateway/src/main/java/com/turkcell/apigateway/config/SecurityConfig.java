b0cde63b5d7dd1c3f1718f6d1d0aa247933ebdf0
emrealkan884 <Hasan.alkan884@gmail.com>
2023-11-29 10:12:39 +0300

    Dockerize v3

--- api-gateway/src/main/java/com/turkcell/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/com/turkcell/apigateway/config/SecurityConfig.java
@@ -2,9 +2,10 @@ package com.turkcell.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
+import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
@@ -20,10 +21,20 @@ public class SecurityConfig {
                     // .pathMatchers("/eureka/**")
                     .pathMatchers("/eureka/**")
                     .permitAll()
+                    .pathMatchers("/api/cars/**")
+                    .authenticated()
                     .anyExchange()
                     .authenticated())
-        .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+        .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtDecoder(customJwtDecoder())));
 
     return serverHttpSecurity.build();
+  }
+
+  @Bean
+  public ReactiveJwtDecoder customJwtDecoder() {
+    // Configure and return your JwtDecoder bean here
+    return NimbusReactiveJwtDecoder.withIssuerLocation(
+            "http://keycloak:8080/realms/rent-a-car-microservice")
+        .build();
   }
 }
